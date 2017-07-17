92356edb8354b2108fb833c9161c31cd4221eca7
sash694 <sashaank@email.arizona.edu>
2017-07-17 15:59:40 -0400

    exr 2

---
+++ day_one/exr_2.py
@@ -0,0 +1,7 @@
+def area_of_room():
+	length= float(input("Enter the length in m of the room: "))
+	width= float(input("Enter the width in m of the room: "))
+	area= length*width
+	print("Area= ", area,'m')
+
+area_of_room()
